61ac9969e1c2c92ba34075818cf2b3ebef7e6e87
davidreese3 <david33reese@gmail.com>
2025-03-13 14:23:57 -0400

    Added graph for hours per year

--- spoti_stats.py
+++ spoti_stats.py
@@ -7,9 +7,13 @@ def main():
     start = time.time()
     df = setUp()
     ta = topAlbums(df)
+    ts = topSongs(df)
+    tpy = timePerYear(df)
     end = time.time()
     topAlbumsPlot(ta)
-    print(f"Data gotten in: {end - start}")
+    topSongsPlot(ts)
+    timePerYearPlot(tpy)
+    print(f"Data calculated in: {end - start}")
 
 def setUp():
     file_location_prefix = "my_spotify_data/Spotify Extended Streaming History/Streaming_History_Audio_"
@@ -31,6 +35,22 @@ def topAlbums(df):
     album_df = album_df.sort_values(by="hours_played", ascending=False).head(10)
     return album_df
 
+"""
+sum of the most played songs, distinguishing by artist and album to account for different songs with the same name. 
+however, it still differentiates between EP and LP versions.
+"""
+def topSongs(df):
+    song = df.groupby(["master_metadata_track_name", "master_metadata_album_album_name", "master_metadata_album_artist_name"]).size().reset_index(name="times_played")
+    song_df = song.reset_index().rename(columns={"master_metadata_track_name" : "song",
+                                                 "master_metadata_album_album_name" : "album",
+                                                 "master_metadata_album_artist_name" : "artist"})
+    song_df = song_df.nlargest(10, "times_played")
+    return song_df
+
+def timePerYear(df):
+    df["year"] = df["ts"].str.split("-").str[0]
+    time = df.groupby(["year"])["ms_played"].sum().div(3600000).reset_index(name="hours_played")
+    return time
 
 def topAlbumsPlot(df):
     df["album_artist"] = df["album"] + " (" + df["artist"] + ")"
@@ -38,6 +58,25 @@ def topAlbumsPlot(df):
     plt.xlabel("Album")
     plt.ylabel("Hours Played")
     plt.title("Top Albums by Hours Played")
+    plt.xticks(rotation=45, ha="right")  
+    plt.tight_layout()
+    plt.show()
+
+def topSongsPlot(df):
+    df["song_artist"] = df["song"] + " (" + df["artist"] + ")"
+    plt.plot(df["song_artist"], df["times_played"], marker="o", linestyle="-")
+    plt.xlabel("Song")
+    plt.ylabel("Times Played")
+    plt.title("Top Songs by Time Played")
+    plt.xticks(rotation=45, ha="right")  
+    plt.tight_layout()
+    plt.show()
+
+def timePerYearPlot(df):
+    plt.plot(df["year"], df["hours_played"], marker="o", linestyle="-")
+    plt.xlabel("Year")
+    plt.ylabel("Hours Played")
+    plt.title("Hours Played Per Year")
     plt.xticks(rotation=45, ha="right")  
     plt.tight_layout()
     plt.show()
