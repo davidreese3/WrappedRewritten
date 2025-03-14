586ed61fb34adf0ef98c89a4cf3736d179a4e2f5
davidreese3 <david33reese@gmail.com>
2025-03-14 17:01:58 -0400

    year and artist keyword to specify data

--- data_loader.py
+++ data_loader.py
@@ -1,18 +1,29 @@
 import pandas as pd
 import os
 
-def load_spotify_data(file_location):
+def load_spotify_data(file_location, year="", artist=""):
     dataframes = []
-    files = getListOfFiles(file_location)
+    files = getListOfFiles(file_location, year)
     for file in files:
         df = pd.read_json(file_location + "/" + file)
         dataframes.append(df)
     combined_df = pd.concat(dataframes, ignore_index=True)
+    if year != "":
+        combined_df["year"] = combined_df["ts"].str.split("-").str[0]
+        combined_df = combined_df[combined_df["year"] == year]
+    if artist != "":
+        combined_df["artist"] = combined_df["master_metadata_album_artist_name"]
+        combined_df = combined_df[combined_df["artist"] == artist]
     return combined_df
 
-def getListOfFiles(file_location):
+def getListOfFiles(file_location, year=""):
         listOfFiles = [file for file in os.listdir(file_location) if os.path.isfile(os.path.join(file_location, file))]
         listOfFilesToRemove = [file for file in listOfFiles if not("Streaming_History_Audio_" in file)]
+        if year != "":
+            for file in listOfFiles:
+                if year not in file and file not in listOfFilesToRemove:
+                    listOfFilesToRemove.append(file)
+        
         for file in listOfFilesToRemove:
              listOfFiles.remove(file)
         return listOfFiles
--- wrapped_rewritten.py
+++ wrapped_rewritten.py
@@ -10,9 +10,14 @@ import report_generator as rg
 import data_loader as dl
 
 def main():
-    df = dl.load_spotify_data("my_spotify_data/Spotify Extended Streaming History")
+    print("Select a year (leave blank for all):")
+    year = input()
+    print("Select an artist (leave blank for all):")
+    artist = input()
+    print("Please wait...")
+    df = dl.load_spotify_data("my_spotify_data/Spotify Extended Streaming History", year, artist)
     start = time.time()
-    rg.generate_report(df)
+    rg.generate_report(df, year)
     end = time.time()
     print(f"Data processed and visualized in: {end - start}")
 
